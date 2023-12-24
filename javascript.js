52756c6da682c15d33c91b3ac8dbb2e7d6642213
Andre Tavora <andre.tavora2@gmail.com>
2023-12-24 19:53:25 -0300

    Incluido Forms de Cadastro e de Login com validacao pelo JS. Obs: Mudar layout na próxima vevrsão!

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+    // Adicione a classe 'navbar-scrolled' ao fazer scroll
+    window.addEventListener('scroll', function() {
+      var navbar = document.querySelector('.navbar');
+      if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+      } else {
+        navbar.classList.remove('navbar-scrolled');
+      }
+    });
+   //Adicionando validaçao do Form de Login e Senha 
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "" || password == "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+}
